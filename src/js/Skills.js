0a011f6b1ef1e7d64680ebee28cf3caac6d25dd7
kyrstenkelly <kyrsten.kelly@trendkite.com>
2017-09-10 20:08:32 -0500

    Expand/hide full list of skills

--- src/js/Skills.js
+++ src/js/Skills.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import _ from 'lodash';
 
 import { skills } from './data';
+import collapseIcon from '../img/collapse-icon.png';
+import expandIcon from '../img/expand-icon.png';
 import starFilled from '../img/star-filled-icon.png';
 import star from '../img/star-icon.png';
 
 class Skills extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false
+    };
+  }
+
+  getSkillList(listOfSkills) {
+    return listOfSkills.map((skill, i) => {
+      return (
+        <div className="skill list-item--small" key={i}>
+          <span className="skill--label">{skill.label}</span>
+          <span className="skill--stars">{this.getStars(skill.rank)}</span>
+        </div>
+      );
+    });
+  }
+
   getStars(rank) {
     const stars = [];
 
@@ -24,21 +45,41 @@ class Skills extends Component {
     return stars;
   }
 
-  render() {
-    const skillList = skills.map((skill, i) => {
-      return (
-        <div className="skill list-item--small" key={i}>
-          <span className="skill--label">{skill.label}</span>
-          <span className="skill--stars">{this.getStars(skill.rank)}</span>
-        </div>
-      );
+  toggleExpandedList() {
+    this.setState({
+      expanded: !this.state.expanded,
     });
+  }
+
+  render() {
+    const initialSkills = skills.slice(0, 12);
+    const expandedSkills = skills.slice(12, skills.length);
+    const initialSkillList = this.getSkillList(initialSkills);
+    const visibleSkillList = this.getSkillList(expandedSkills);
+    const icon = this.state.expanded ? collapseIcon : expandIcon;
+    const expandedClass = this.state.expanded ? 'active' : '';
 
     return (
       <div className="skills-container">
         <div className="title">Skills</div>
         <div className="skills">
-          {skillList}
+          <div className="skills--initial">
+            {initialSkillList}
+          </div>
+          <div className={`skills--expanded ${expandedClass}`}>
+            {visibleSkillList}
+          </div>
+          <div style={{ float:"left", clear: "both" }}
+            ref={(el) => { this.skillsEnd = el; }}>
+          </div>
+        </div>
+        <div className="skills-expand">
+          <img
+            className="icon"
+            src={icon}
+            onClick={() => {this.toggleExpandedList()}}
+            alt="toggle list"
+          />
         </div>
       </div>
     );
